9f419da0394437a4b0dc44091e20125bd870abec
Roshan Khatri <khatriroshan425@gmail.com>
2025-10-04 05:03:04 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,6 +7,7 @@ export const navLinks = [
   { href: '/', label: 'Home' },
   { href: '/about', label: 'About Us' },
   { href: '/gallery', label: 'Gallery' },
+  { href: '/contact', label: 'Contact' },
 ];
 
 export const socialLinks = [
