7e6abdb349a1e33b059fb2ec8696be52634297eb
Roshan Khatri <khatriroshan425@gmail.com>
2025-10-11 19:50:07 +0000

    favicon  icon changed boxing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ const playfairDisplay = Playfair_Display({
   variable: '--font-playfair-display',
 });
 
+const boxingGloveSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22a2 2 0 0 0 2-2v-2H8v2c0 1.1.9 2 2 2Z"/><path d="M16 18H6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h2.57a3 3 0 0 1 2.39 1.08L14 10h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-2"/><path d="M9.5 8.5v-1a2 2 0 0 1 2-2v0a2 2 0 0 1 2 2v2"/><path d="M12.5 6.5v-1a1 1 0 0 1 1-1h1"/><path d="M16 10h1.5a1 1 0 0 1 1 1v1"/></svg>`;
+const faviconDataUri = `data:image/svg+xml;base64,${btoa(boxingGloveSvg)}`;
+
 export const metadata: Metadata = {
   title: {
     default: 'JSB Boxing Academy - Punch for Hope',
@@ -26,7 +29,7 @@ export const metadata: Metadata = {
   },
   description: 'JSB Boxing Academy - Training Champions, Building Futures',
   icons: {
-    icon: '/favicon.ico',
+    icon: faviconDataUri,
   },
 };
 
